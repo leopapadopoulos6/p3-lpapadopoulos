52f6e8f49b379fbddfc64c35df69146e51dd7c3b
leopapadopoulos6 <90059812+leopapadopoulos6@users.noreply.github.com>
2021-09-17 02:20:30 -0400

    Add files via upload

---
+++ p3-lpapadopoulos.py
@@ -0,0 +1,12 @@
+"""
+Leonidas Papadopoulos
+homework 1
+
+taking user input and displaying it
+"""
+
+first = input("Please tell me your first name: ")
+last = input("Please tell me your last name: ")
+name = first + " " + last
+print("Hello", name)
+
